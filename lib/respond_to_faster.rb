8892e2f34e83e8a8cbc8718706986e06a1c42c6c
Chris Salzberg <chris@dejimata.com>
2017-12-18 21:27:25 +0900

    Add code

--- lib/respond_to_faster.rb
+++ lib/respond_to_faster.rb
@@ -1,5 +1,15 @@
 require "respond_to_faster/version"
 
 module RespondToFaster
-  # Your code goes here...
+  def init_with(coder)
+    super.tap do
+      (attribute_names - self.class.attribute_names).each do |name|
+        singleton_class.define_attribute_method name
+      end
+    end
+  end
 end
+
+ActiveRecord::Base.include RespondToFaster
+ActiveModel::AttributeMethods.send(:remove_method, :respond_to?)
+ActiveModel::AttributeMethods.send(:remove_method, :method_missing)
