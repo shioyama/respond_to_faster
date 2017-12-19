1763c74ee55820a16f6a6fd0763ab27127eecf74
Chris Salzberg <chris@dejimata.com>
2017-12-19 12:25:47 +0900

    Cache modules across singleton classes

--- lib/respond_to_faster.rb
+++ lib/respond_to_faster.rb
@@ -4,8 +4,15 @@ require "respond_to_faster/version"
 module RespondToFaster
   def init_with(coder)
     super.tap do
-      (attribute_names - self.class.attribute_names).each do |name|
-        singleton_class.define_attribute_method name
+      unless (uncached_attrs = attribute_names - self.class.attribute_names).empty?
+        klass = self.class
+        mod_name = "RespondToFaster_#{uncached_attrs.hash.abs}".freeze
+        if klass.const_defined?(mod_name)
+          singleton_class.include klass.const_get(mod_name)
+        else
+          uncached_attrs.each &singleton_class.method(:define_attribute_method)
+          klass.const_set(mod_name, singleton_class.send(:generated_attribute_methods))
+        end
       end
     end
   end
