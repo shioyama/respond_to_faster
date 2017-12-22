3d05c0e9b50a07612ec1edf83fae7f0bcea724db
Chris Salzberg <chris@dejimata.com>
2017-12-22 14:15:56 +0900

    Override find_by_sql instead of init_with
    Overriding init_with results in extra cycles burned for every record
    instantiated. For the case we're optimizing for, where we're selecting
    results with one or more aliased columns, it makes more sense to do the
    extra processing *once* and then only include the created module into
    each instance.
    
    If no method in this module is ever used, then the actual processing
    time will actually increase slightly over the case where we do not
    include a module into the singleton class; this is expected since we're
    doing extra work. However, if we call any method on the module at least
    once per instance, which is typically the use case, then performance is
    improved. This improvement is proportional to the number of times we
    call the methods on the included module (i.e. column aliases).

--- lib/respond_to_faster.rb
+++ lib/respond_to_faster.rb
@@ -2,22 +2,18 @@ require "active_record"
 require "respond_to_faster/version"
 
 module RespondToFaster
-  def init_with(coder)
-    super.tap do
-      unless (uncached_attrs = attribute_names - self.class.attribute_names).empty?
-        klass = self.class
-        mod_name = "RespondToFaster_#{uncached_attrs.hash.abs}".freeze
-        if klass.const_defined?(mod_name)
-          singleton_class.include klass.const_get(mod_name)
-        else
-          uncached_attrs.each &singleton_class.method(:define_attribute_method)
-          klass.const_set(mod_name, singleton_class.send(:generated_attribute_methods))
-        end
+  def find_by_sql(sql, binds = [], preparable: nil, &block)
+    super.tap do |records|
+      record = records.first
+      unless (uncached_attrs = record.attribute_names - attribute_names).empty?
+        uncached_attrs.each &record.singleton_class.method(:define_attribute_method)
+        mod = record.singleton_class.send(:generated_attribute_methods)
+        records[1..-1].each { |record| record.singleton_class.include mod }
       end
     end
   end
 end
 
-ActiveRecord::Base.include RespondToFaster
+ActiveRecord::Base.extend RespondToFaster
 ActiveModel::AttributeMethods.send(:remove_method, :respond_to?)
 ActiveModel::AttributeMethods.send(:remove_method, :method_missing)
