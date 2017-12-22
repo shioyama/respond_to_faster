236ede82ecc17f9fb3451615abf849f8fd661335
Chris Salzberg <chris@dejimata.com>
2017-12-22 14:57:22 +0900

    Only call define_method_attribute

--- lib/respond_to_faster.rb
+++ lib/respond_to_faster.rb
@@ -6,7 +6,7 @@ module RespondToFaster
     super.tap do |records|
       record = records.first
       unless record.nil? || (uncached_attrs = record.attribute_names - attribute_names).empty?
-        uncached_attrs.each &record.singleton_class.method(:define_attribute_method)
+        uncached_attrs.each &record.singleton_class.method(:define_method_attribute)
         mod = record.singleton_class.send(:generated_attribute_methods)
         records[1..-1].each { |record| record.singleton_class.include mod }
       end
